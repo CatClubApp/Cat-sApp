3198644b8f1d393c32d138ae327f8b31893b33e6
Mas Agung <57654160+Agung78@users.noreply.github.com>
2021-02-04 18:33:13 +0700

    google

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,10 @@ const {authenticate} = require('../middleware/middleware')
 app.post('/login', Controller.login)
 app.post('/register', Controller.register)
 
+
+app.post('/googlelogin', Controller.googlelogin)
+app.post('/googleregister', Controller.googleregister)
+
 app.use(authenticate)
 app.get('/home', Controller.home)
 
