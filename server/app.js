375dccdaae1c6884ba2131924c29408012cc9951
Mas Agung <57654160+Agung78@users.noreply.github.com>
2021-02-04 15:26:51 +0700

    Routes

---
+++ server/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const { json } = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const routes = require('./routes/index')
+
+app.use(json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+app.use(routes)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
