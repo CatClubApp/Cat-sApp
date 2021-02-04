fdcd453b601402fdb9d0d0c53dbfbb279b84dc7c
EnrikoAviyantoPutra <enriko.aviyanto@gmail.com>
2021-02-04 16:15:56 +0700

    update di development

--- server/app.js
+++ server/app.js
@@ -5,13 +5,11 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const routes = require('./routes/index')
-
 app.use(json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use(routes)
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,9 @@ const app = express.Router()
 const Controller = require('../controller/index')
 const {authenticate} = require('../middleware/middleware')
 
-app.get('/home', Controller.home)
 app.use(authenticate)
-app.get('/login', Controller.login)
-app.get('/register', Controller.register)
+app.get('/home', Controller.home)
+app.post('/login', Controller.login)
+app.post('/register', Controller.register)
 
 module.exports = app
